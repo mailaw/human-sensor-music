d2500b8ca29eca46c213cf4afdba967a1f50194c
mailaw <mlaw534@gmail.com>
2018-11-02 17:01:27 -0400

    added sound library n samples

--- sensorInput/sensorInput.js
+++ sensorInput/sensorInput.js
@@ -1,6 +1,13 @@
 var serial;
 var portName = '/dev/cu.usbmodem1421';
 var inData; //sensor values
+var sample1, beat1, strings1;
+
+function preload() {
+  sample1 = loadSound('samples/DOLCE_VITA.wav');
+  beat1 = loadSound('samples/yae_beat.wav');
+  strings1 = loadSound('samples/grand_strings.wav');
+}
 
 function setup() {
   createCanvas(400,400);
@@ -16,6 +23,7 @@ function setup() {
   
   serial.list();
   serial.open(portName); //open port
+ 
 }
 
 function serialEvent() { //responds to new data
@@ -27,11 +35,14 @@ function draw() {
   background("#2307AF");
   fill(255);
   // ellipse(sensorValue,height/2,20,20);
+  text("sensor", 20, 20);
   if (inData == 1 || inData == 0){
-    text("sensor value: " + inData, 20, 20);
+    text("sensor value: " + inData, 80, 20);
   }
+  pirSound(inData);
 }
 
+///PORT FUNCTIONS
 function printList(portList){
   for (var i = 0; i < portList.length; i++){
     //display list the ocnsole
@@ -53,4 +64,16 @@ function serialError(err) {
  
 function portClose() {
   console.log('The serial port closed.');
+}
+
+//SOUND FUNCTIONS
+function pirSound(inData){
+  if(inData == 1){
+    beat1.play();
+    strings1.play();
+    background(0,255,0);
+  } else {
+    beat1.stop();
+    background("#2307AF");
+  }
 }
